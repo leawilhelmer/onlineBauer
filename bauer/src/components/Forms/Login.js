6a0fcf4a59dc712e7b778fa2ee8c9e9ab53292e0
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-03-30 10:56:47 -0700

    added login and signup

---
+++ bauer/src/components/Forms/Login.js
@@ -0,0 +1,86 @@
+import React, { useState, useContext, useEffect } from "react";
+import { Redirect, useHistory } from "react-router-dom";
+import axios from "axios";
+import "../Forms/styles.css";
+import { loginUser } from "../../utils/API";
+import UserContext from "../../Context/UserContext";
+
+// Login component
+const Login = () => {
+  // Set the state for the login Details
+  const [loginDetails, setLoginDetails] = useState({
+    email: "",
+    password: "",
+  });
+  // Set user data to the userContext
+  const { userData, setUserData } = useContext(UserContext);
+  // use history to be able to redirect people
+  const history = useHistory();
+
+  // Use effect to see if the user is logged in
+  useEffect(() => {
+    console.log(userData);
+    // If the user is logged in, send them to the map
+    if (userData.token) history.push("/");
+  }, [userData.token, history]);
+
+  const onChange = (e) => {
+    setLoginDetails({ ...loginDetails, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      // console.log(loginDetails, "FROM LOGIN.js");
+      loginUser(loginDetails).then((res) => {
+        // set the user data to the token, and the user info
+        setUserData({
+          token: res.data.token,
+          user: res.data.user,
+        });
+        // Set the local storage token
+        localStorage.setItem("auth-token", res.data.token);
+        // After login, send to the map
+        return history.push("/");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // Return the component
+  return (
+    <div className="login-form">
+      <h2>Log In</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email address</label>
+          <br></br>
+          <input
+            onChange={onChange}
+            type="email"
+            name="email"
+            className="form-control"
+            id="email-input"
+            placeholder="Email"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <br></br>
+          <input
+            onChange={onChange}
+            type="password"
+            name="password"
+            className="form-control"
+            id="password-input"
+            placeholder="Password"
+          ></input>
+        </div>
+        <button type="submit" className="btn btn-primary login">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ bauer/src/components/Forms/SignUp.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const SignUp = () => {
+  //use onSubmit
+  const [signUpForm, setSignUpForm] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e) => {
+    setSignUpForm({ ...signUpForm, [e.target.name]: e.target.value });
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      // console.log("submit runs");
+      await axios.post("/api/user", signUpForm);
+      console.log(signUpForm, "user info");
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="signup-form">
+      <h2>Sign Up</h2>
+      <form onSubmit={submit}>
+        <div className="form-group">
+          <label>Name</label>
+          <br></br>
+          <input
+            type="text"
+            name="firstName"
+            className="form-control"
+            id="firstName-input"
+            placeholder="First Name"
+            onChange={onChange}
+          ></input>
+          <input
+            type="text"
+            name="lastName"
+            className="form-control"
+            id="lastName-input"
+            placeholder="Last Name"
+            onChange={onChange}
+          ></input>
+        </div>
+        <label>User Name</label>
+        <br></br>
+        <input
+          type="text"
+          name="username"
+          className="form-control"
+          id="username-input"
+          placeholder="user name"
+          onChange={onChange}
+        ></input>
+        <div className="form-group">
+          <label>Email address</label>
+          <br></br>
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            id="email-input"
+            placeholder="Email"
+            onChange={onChange}
+          ></input>
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <br></br>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="password-input"
+            placeholder="Password"
+            onChange={onChange}
+          ></input>
+        </div>
+        <button type="submit" className="btn btn-primary signup">
+          Create account
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
